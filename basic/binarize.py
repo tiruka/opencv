3f149b422524da32e6b9f3f7269deebdc8f00f99
tnagai <nagai@zetacx.com>
2020-05-04 13:19:50 +0900

    add basic

---
+++ basic/binarize.py
@@ -0,0 +1,19 @@
+# binarization of black and white
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('img/dolphin.jpeg', 0)
+
+threshold = 100
+ret, img_th = cv2.threshold(img, threshold, 255, cv2.THRESH_BINARY)
+cv2.imshow('th', img_th)
+
+# auto by otsu
+ret, img_o = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+cv2.imshow('otsu', img_o)
+
+# adaptive threshold
+img_ad = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 1)
+cv2.imshow('adaptive', img_ad)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- basic/gamma.py
+++ basic/gamma.py
@@ -1,2 +1,15 @@
-# gamma change
+# gamma convert to change brightness
 import cv2
+import numpy as np
+
+gamma = 0.4 # over 1 -> more bright, under 1 darker
+img = cv2.imread('img/dolphin.jpeg')
+gammva_cvt = np.zeros((256, 1), dtype=np.uint8)
+for i in range(256):
+    gammva_cvt[i][0] = 255 * (float(i) / 255) ** (1.0 / gamma)
+
+img_gamma = cv2.LUT(img, gammva_cvt)
+cv2.imshow('original', img)
+cv2.imshow('gamma', img_gamma)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
